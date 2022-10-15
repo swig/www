2f0edd90b2a20fcef9c752071684aa5f9c698c2d
William S Fulton <wsf@fultondesigns.co.uk>
2022-10-15 17:14:14 +0100

    Remove tabs to fix whitespace consistency

--- makeweb.py
+++ makeweb.py
@@ -10,101 +10,97 @@ import time
 import stat
 
 def makepage(filename, extension):
-        name, suffix = os.path.splitext(filename)
-	f = open("default.html")
-	page = f.read()
+    name, suffix = os.path.splitext(filename)
+    f = open("default.html")
+    page = f.read()
+    f.close()
+
+    # Read in the body file
+    f = open(filename)
+    body = f.readlines()
+    f.close()
+    title = body[0]
+    body = string.join(body[1:])
+    page = string.replace(page,"$title",title + "  ")
+    page = string.replace(page,"$body",body)
+
+    # Read in the corner file
+    try:
+        f = open(name+".corner")
+        corner = f.read()
         f.close()
+    except IOError:
+        try:
+            f = open("default.corner")
+            corner = f.read()
+            f.close()
+        except:
+            corner = ""
+    
+    page = string.replace(page,"$corner",corner)
 
-	# Read in the body file
-        f = open(filename)
-	body = f.readlines()
-	f.close()
-	title = body[0]
-	body = string.join(body[1:])
-	page = string.replace(page,"$title",title + "  ")	
-	page = string.replace(page,"$body",body)
+    # Read in the top file
+    try:
+        f = open(name+".top")
+        top = f.read()
+        f.close()
+    except IOError:
+        try:
+            f = open("default.top")
+            top = f.read()
+            f.close()
+        except:
+            top = ""
+    
+    page = string.replace(page,"$top",top)
 
-	# Read in the corner file
-	try:
-		f = open(name+".corner")
-		corner = f.read()
-		f.close()
-	except IOError:
-		try:
-			f = open("default.corner")
-			corner = f.read()
-			f.close()
-		except:
-			corner = ""
-	
-	page = string.replace(page,"$corner",corner)
+    # Read in the side file
+    try:
+        f = open(name+".side")
+        side = f.read()
+        f.close()
+    except IOError:
+        try:
+            f = open("default.side")
+            side = f.read()
+            f.close()
+        except:
+            side = ""
+    
+    page = string.replace(page,"$side",side)
 
-	# Read in the top file
-	try:
-		f = open(name+".top")
-		top = f.read()
-		f.close()
-	except IOError:
-		try:
-			f = open("default.top")
-			top = f.read()
-			f.close()
-		except:
-			top = ""
-	
-	page = string.replace(page,"$top",top)
+    # Read in the footer file
+    try:
+        f = open(name+".footer")
+        footer = f.read()
+        f.close()
+    except IOError:
+        try:
+            f = open("default.footer")
+            footer = f.read()
+            f.close()
+        except:
+            footer = ""
 
-	# Read in the side file
-	try:
-		f = open(name+".side")
-		side = f.read()
-		f.close()
-	except IOError:
-		try:
-			f = open("default.side")
-			side = f.read()
-			f.close()
-		except:
-			side = ""
-	
-	page = string.replace(page,"$side",side)
+    page = string.replace(page,"$footer",footer)
 
-	# Read in the footer file
-	try:
-		f = open(name+".footer")
-		footer = f.read()
-		f.close()
-	except IOError:
-		try:
-			f = open("default.footer")
-			footer = f.read()
-			f.close()
-		except:
-			footer = ""
+    mtime = os.stat(filename)[stat.ST_MTIME]
+    mstr = time.ctime(mtime)
 
-	page = string.replace(page,"$footer",footer)
+    page = string.replace(page,"$mtime",mstr);
 
-	mtime = os.stat(filename)[stat.ST_MTIME]
-	mstr = time.ctime(mtime)
-
-	page = string.replace(page,"$mtime",mstr);
-
-	# Write out the page
-	f = open(name+"."+extension,"w")
-	f.write(page)
-	f.close()
-	print "Wrote ", name+"."+extension
+    # Write out the page
+    f = open(name+"."+extension,"w")
+    f.write(page)
+    f.close()
+    print "Wrote ", name+"."+extension
 
 files = glob.glob("*.ht")
 
 for f in files:
-	makepage(f, "html")
+    makepage(f, "html")
 
 files = glob.glob("*.ph")
 
 for f in files:
-	makepage(f, "php")
-
-
-
-
+    makepage(f, "php")
